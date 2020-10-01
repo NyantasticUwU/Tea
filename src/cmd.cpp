ff1d6d2a660c806b18b07e22f4a4890c37c467d2
NyantasticUwU <NyantasticUwU@gmail.com>
2020-09-30 21:38:22 -0500

    Throw error if file not given

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -1,7 +1,13 @@
 #include "cmd.hpp"
+#include <iostream>
 
 // This function is used to extract the filename of the .tea file
 char *&getFilename(const int &argc, char **&argv)
 {
+    if (argc <= 1)
+    {
+        std::cout << "ERROR: No file given!";
+        std::exit(1);
+    }
     return argv[1];
 }
