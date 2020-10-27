bf6e1ff62fd4d44ae5cd9a7293cf5bb5c7afbaa9
NyantasticUwU <NyantasticUwU@gmail.com>
2020-10-27 14:09:34 -0500

    Fix open files

--- src/parsefile.cpp
+++ src/parsefile.cpp
@@ -13,10 +13,12 @@ void parseFile(const char *&filename, std::vector<std::string> &file)
     // Creating and opening input file stream
     f.open(filename);
     if (!f)
+    {
         f.close();
         teaError("ERROR: The given file was unable to be opened.\n"
                  "Exiting with code 110 (ERROR_OPEN_FAILED).",
                  110); // Windows' ERROR_OPEN_FAILED (110)
+    }
     // Reading data from file into the file string vector (line by line)
     while (f)
     {
