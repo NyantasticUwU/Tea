80bd2f9dd81eb53adb7c191a67375fbed388330d
NyantasticUwU <NyantasticUwU@gmail.com>
2020-10-27 14:07:39 -0500

    Format error calls

--- src/fileIO.cpp
+++ src/fileIO.cpp
@@ -7,9 +7,7 @@ void checkMainFile(const char *&filename)
 {
     // Making sure the file is regular (not a directory or anything crazy)
     if (!std::filesystem::is_regular_file(static_cast<const std::filesystem::path>(filename)))
-    {
         teaError("ERROR: The given file name doesn't seem to be a regular file.\n"
                  "Exiting with code 222 (ERROR_BAD_FILE_TYPE).",
                  222); // Windows' ERROR_BAD_FILE_TYPE (222)
-    }
 }
